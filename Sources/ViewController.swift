4ee770d21e055908af34baeab648c5ae3c57c034
Klaas Pieter Annema <kpa@annema.me>
2016-12-24 22:00:20 +0100

    Add record button

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -2,19 +2,29 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+  var shouldRecord: Bool = false
+
   @IBOutlet var textField: NSTextField!
   @IBOutlet var label: NSTextField!
+
+  @IBOutlet var recordButton: NSButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     view.wantsLayer = true
     view.layer?.backgroundColor = NSColor.white.cgColor
+
+    shouldRecord = recordButton.state == NSOnState
   }
 
   override func viewDidAppear() {
     super.viewDidAppear()
     view.window?.makeFirstResponder(textField)
+  }
+
+  @IBAction func toggleRecording(_ sender: Any) {
+    shouldRecord = recordButton.state == NSOnState
   }
 }
 
